aedf2a9f75aeeef4a2a95625a30a819d01ad8544
Stepan Kostusiev <ironclimber@gmail.com>
2019-12-10 15:59:49 +0200

    Ex 11.6-11.7

---
+++ 11/exercises/ex_11_06_swap/swap.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+string punctuation = ".;,?-'";
+
+bool is_punctuation(char ch) {
+    for (char c : punctuation) {
+        if (ch == c) { return true; }
+    }
+    return false;
+}
+
+
+int main() {
+
+    while (cin) {
+
+        //get line
+        cout<<"Type some string:"<<endl;
+        string line, newline;
+        getline(cin, line);
+        cout<<endl<<"Result:"<<endl; 
+
+        bool shield = false;
+
+        for (char ch : line) {
+            if (is_punctuation(ch) && !shield) { newline += ' '; }
+            else {
+                newline += ch;
+                if (ch=='"') { shield = !shield; }
+            }      
+        }
+
+        cout << newline << endl << endl;
+    }
+
+
+    return 0;
+
+}
